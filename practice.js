bbf1f15ba51d82e58a1a91d60d4a38e9623ceb3a
Fredrick Jones <fredrick.w.jones@gmail.com>
2015-01-14 12:25:03 -0700

    finished

--- practice.js
+++ practice.js
@@ -4,23 +4,23 @@ var outer = function(){
   var name = 'Tyler';
   return function(){
     return 'The original name was ' + name;
-  }
-}
+  };
+};
+
 //Above you're given a function that returns another function which has a closure over the name variable.
 //Invoke outer saving the return value into another variable called 'inner'.
 
   //Code Here
+var inner = outer();
 
 //Once you do that, invoke inner.
 
   //Code Here
+inner();
 
 
 
 //Next problem
-
-
-
 var callFriend = function(){
   var friend = 'Jake';
   function callF(number){
@@ -33,18 +33,28 @@ var callFriend = function(){
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
   //Code Here
+var friendNum = callFriend();
 
 
 
 //Next Problem
-
-
 
 /*
   Write a function called makeCounter that makes the following code work properly.
 */
 
   //Code Here
+var makeCounter = function() {
+  var counter = 0;
+  return function() {
+    counter += 1;
+    return counter;
+  };
+};
+
+var count = makeCounter();
+count();
+
   var count = makeCounter();
   count() // 1
   count() // 2
@@ -55,15 +65,47 @@ var callFriend = function(){
 
 //Next Problem
 
-
-
 /*
   Write a function that accepts another function as it's only argument and returns a new function
   (which invokes the original function that was passed in) that can only ever be executed once.
   Once completed, add a second arguments that allows the function to be invoked N number of times.
   After the function has been called N number of times, console.log('STAHHP');
 */
+var outerFn = function(fn) {
+  var called = false;
+  return function() {
+    if(called === false) {
+      fn();
+      called = true;
+    };
+  };
+};
 
+var innerFn = outerFn(function() {
+  console.log('ONCE');
+});
+
+innerFn();
+
+//Part Two
+var outerFn = function(fn, N) {
+  var i = N;
+  return function() {
+    if(i > 0) {
+      fn();
+      i--;
+    }
+    else {
+      console.log('STAHHP');
+    };
+  };
+};
+
+var innerFn = outerFn(function() {
+  console.log('ONCE');
+}, 5);
+
+innerFn();
 
 
 
